9eea9915dd9ea5cd8cbe50c8ab200063b9229d20
Aryanvaish <108726947+Aryanvaish@users.noreply.github.com>
2025-11-17 22:54:22 +0530

    Vercel Analytics added..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Intro from "@/components/intro";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
-    <Intro />
+    <>
+      <Intro />
+      <Analytics />
+    </>
   );
 }
